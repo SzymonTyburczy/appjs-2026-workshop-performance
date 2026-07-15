2ada7facca72a578ed6e82d2b6583bbdb1f9e934
Szymon Tyburczy <SzymonTyburczy@protonmail.com>
2026-07-15 17:25:36 +0200

    1.2 performance shimmers update

--- src/components/feed/shimmer/image-with-shimmer.tsx
+++ src/components/feed/shimmer/image-with-shimmer.tsx
@@ -1,29 +1,22 @@
-import { useState } from "react";
 import { Image, ImageProps } from "expo-image";
 import { StyleProp, View, ViewStyle } from "react-native";
 
-import { ImageShimmer } from "./image-shimmer";
+const DEFAULT_BLURHASH = "L6PZfSi_.AyE_3t7t7R**0o#DgR4";
 
 export const ImageWithShimmer = ({
   style,
   contentFit = "cover",
-  onLoad,
   ...imageProps
 }: ImageProps) => {
-  const [isLoaded, setIsLoaded] = useState(false);
-
   return (
     <View style={[style as StyleProp<ViewStyle>, { overflow: "hidden" }]}>
       <Image
         {...imageProps}
         style={style}
         contentFit={contentFit}
-        onLoad={(event) => {
-          setIsLoaded(true);
-          onLoad?.(event);
-        }}
+        placeholder={{ blurhash: DEFAULT_BLURHASH }}
+        transition={200}
       />
-      {!isLoaded && <ImageShimmer />}
     </View>
   );
 };
